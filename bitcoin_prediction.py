1b17629c23abe9581cc10046072bc45749742df7
michaelba8 <63546875+michaelba8@users.noreply.github.com>
2021-02-03 14:39:26 +0200

    fix text file

--- bitcoin_prediction.py
+++ bitcoin_prediction.py
@@ -5,17 +5,46 @@ import datetime as dt
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import numpy as np
+import csv
+import math
+import matplotlib.pyplot as plt
+from numpy import linalg as LA
 
 
 
 def main():
-    df=get_bars()
-    print(df)
-    X=np.float64(df)
-    print(X.shape)
+    result=read_data()
 
 
 
+def read_data(months=2):
+    const="data/BTCTUSD-1m-"
+    year=2020
+    month=10
+    s_month=str(10)
+    count=1
+    reader = csv.reader(open("data/BTCTUSD-1m-2020-10.csv", "rt"), delimiter=",")
+    temp = list(reader)
+    result = np.array(temp).astype("float")
+    while(count<months):
+        month-=1
+        if(month<1):
+            month=12
+            year-=1
+
+        if(month<10):
+            s_month="0"+str(month)
+        else:
+            s_month=str(month)
+        s=const+str(year)+"-"+s_month+".csv"
+        reader = csv.reader(open(s, "rt"), delimiter=",")
+        temp = list(reader)
+        res = np.array(temp).astype("float64")
+        result=np.vstack((res,result))
+        count+=1
+        print("append file",s)
+    return result
 
 
 def get_bars(symbol="BTCUSDT", interval="1m", limit="1500"):
